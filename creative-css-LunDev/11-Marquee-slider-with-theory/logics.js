1dc48a32a323794abcf7e5f5d5850b0593141d5f
Azizul <khanazizul1199@gmail.com>
2025-01-09 14:14:08 +0600

    for new instance better use Array.from() method

--- creative-css-LunDev/11-Marquee-slider-with-theory/logics.js
+++ creative-css-LunDev/11-Marquee-slider-with-theory/logics.js
@@ -1,6 +1,6 @@
 // Marquee slider logics
 const list = document.querySelector(".list");
 list.style.setProperty("--quantity", list.childElementCount);
-[...list.children].forEach((element, indx) => {
+Array.from(list.children).forEach((element, indx) => {
     element.style.setProperty("--position", indx);
 });
